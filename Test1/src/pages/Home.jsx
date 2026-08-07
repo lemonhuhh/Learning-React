6923fff4980c7f2d33c5d3cd8bd1a7a1267c826b
lemonhuhh <tamangprabin711@gmail.com>
2026-08-07 12:11:52 +0545

    Added product card

--- Test1/src/pages/Home.jsx
+++ Test1/src/pages/Home.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from "react";
 
 function Home() {
+  const Products = [
+    {
+      id: 1,
+      name: "Jacket",
+      price: 5000,
+    },
+    {
+      id: 2,
+      name: "Jeans",
+      price: 3000,
+    },
+  ];
+
   return (
     <div>
-      This is Home page
+      {Products.map((product) => {
+          <div key={product.id}>
+            <h3>{product.name}</h3>
+            <p>Price: ${product.price}</p>
+          </div>
+      })}
     </div>
-  )
+  );
 }
 
-export default Home
+
+
+export default Home;
